8d88c5d94622d1796a174e9c46b210661588f690
selvanand k <selvanandk@gmail.com>
2019-06-25 07:04:11 +0530

    Commit for 1. Designing Wish corner 2. Few ui alignments in home page

--- app/src/main/java/android/adwait/com/wish_corner/view/ADWishCornerFragment.kt
+++ app/src/main/java/android/adwait/com/wish_corner/view/ADWishCornerFragment.kt
@@ -4,6 +4,7 @@ import and.com.polam.utils.ADBaseActivity
 import android.adwait.com.R
 import android.adwait.com.utils.ADBaseFragment
 import android.os.Bundle
+import android.support.design.widget.TabLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,7 +12,11 @@ import kotlinx.android.synthetic.main.fragment_wish_corner.*
 
 class ADWishCornerFragment : ADBaseFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater?.inflate(R.layout.fragment_wish_corner, container, false)
 
         return view
@@ -19,6 +24,32 @@ class ADWishCornerFragment : ADBaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        (activity as ADBaseActivity).showMessage(getString(R.string.development), wish_parent, false)
+
+
+
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.upcoming))
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.recent))
+        tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+        })
+
+
+        (activity as ADBaseActivity).showMessage(
+            getString(R.string.development),
+            wish_parent,
+            false
+        )
     }
 }
